942fa75c6b2db0110fad62d4194a24c7f45239ed
Ward Peeters <ward@coding-tech.com>
2020-01-22 22:45:49 +0100

    add typescript types & generation

---
+++ packs/angular.d.ts
@@ -0,0 +1,9 @@
+export const icon: "data:image/svg+xml,%3Csvg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 250 250\"%3E%3Cpath fill=\"%23dd0031\" d=\"M125 30L31.9 63.2l14.2 123.1L125 230l78.9-43.7 14.2-123.1z\"/%3E%3Cpath fill=\"%23c3002f\" d=\"M125 30v22.2-.1V230l78.9-43.7 14.2-123.1L125 30z\"/%3E%3Cpath d=\"M125 52.1L66.8 182.6h21.7l11.7-29.2h49.4l11.7 29.2H183L125 52.1zm17 83.3h-34l17-40.9 17 40.9z\" fill=\"%23fff\"/%3E%3C/svg%3E";
+export namespace UIStrings {
+    export const total_byte_weight: string;
+    export const unminified_warning: string;
+    export const unused_javascript: string;
+    export const uses_responsive_images: string;
+    export const uses_rel_preload: string;
+    export const dom_size: string;
+}
