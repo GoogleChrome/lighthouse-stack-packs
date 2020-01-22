3ede9157e92001f86153ad7f2cfc4e38b70e7e69
Ward Peeters <ward@coding-tech.com>
2020-01-22 22:45:49 +0100

    add typescript types & generation

---
+++ packs/amp.d.ts
@@ -0,0 +1,9 @@
+export const icon: "data:image/svg+xml,%3Csvg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 256 256\"%3E%3Cpath d=\"M171.887 116.28l-53.696 89.36h-9.728l9.617-58.227-30.2.047a4.852 4.852 0 01-4.855-4.855c0-1.152 1.07-3.102 1.07-3.102l53.52-89.254 9.9.043-9.86 58.317 30.413-.043a4.852 4.852 0 014.855 4.855c0 1.088-.427 2.044-1.033 2.854l.004.004zM128 0C57.306 0 0 57.3 0 128s57.306 128 128 128 128-57.306 128-128S198.7 0 128 0z\" fill=\"%230379c4\" fill-rule=\"evenodd\"/%3E%3C/svg%3E";
+export namespace UIStrings {
+    export const uses_webp_images: string;
+    export const offscreen_images: string;
+    export const render_blocking_resources: string;
+    export const unminified_css: string;
+    export const efficient_animated_content: string;
+    export const uses_responsive_images: string;
+}
