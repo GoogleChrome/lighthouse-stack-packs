142b8b49c35394b394c028785863f7324f89433a
Paul Irish <commits@paul.irish>
2020-12-08 12:42:44 -0800

    tests good

--- test/lh-compat-test.js
+++ test/lh-compat-test.js
@@ -1,5 +1,27 @@
-const {stackPacksToInclude: lhStackPacks} = require('lighthouse/lib/stack-packs.js');
+const {test} = require('uvu');
+const assert = require('uvu/assert');
 
-const (const lhStackPack of lhStackPacks) {
-  // todo
-}
+const {stackPacksToInclude: lhStackPackRefs} = require('lighthouse/lighthouse-core/lib/stack-packs.js');
+
+const packs = require('../');
+
+test('all LH referenced packs exist here', () => {
+  for (const lhStackPack of lhStackPackRefs) {
+    const matchedPack = packs.find(pack => pack.id === lhStackPack.packId);
+    assert.ok(matchedPack, `pack for ${lhStackPack.packId} found here`);
+    assert.equal(matchedPack.id, lhStackPack.packId);
+  }
+});
+
+test('all packs here are included there', () => {
+  for (const pack of packs) {
+    const matchedObj = lhStackPackRefs.find(lhStackPackRef => lhStackPackRef.packId === pack.id);
+    if (!matchedObj) {
+      // New packs will land here first, so it's not fair to call these errors...
+      // But after rolling (and js-library-detector support) we expect all packs to be supported in LH
+      console.warn(`Pack (${pack.id}) is not yet referenced in lighthouse's lib/stack-packs.`);
+    }
+  }
+});
+
+test.run();
--- test/module-test.js
+++ test/module-test.js
@@ -1,21 +1,34 @@
+const {test} = require('uvu');
+const assert = require('uvu/assert');
+const fs = require('fs');
 
-const assert = require('assert');
 const thisModulesPacks = require('../');
 
+test('all authored packs are refernced in module', () => {
+  const allFilenames = fs.readdirSync(__dirname + '/../packs');
+  const allPackModules = allFilenames.map(filename => require(`${__dirname}/../packs/${filename}`));
+  const allPackModuleIds = allPackModules.map(pack => pack.id);
 
+  const referencedPackIds = thisModulesPacks.map(pack => pack.id);
+  assert.equal(allPackModuleIds.sort(), referencedPackIds.sort());
+});
 
-assert.ok(Array.isArray(thisModulesPacks));
-assert.ok(thisModulesPacks.length)
+test('packs are well-structured', () => {
+  assert.ok(Array.isArray(thisModulesPacks));
+  assert.ok(thisModulesPacks.length);
 
-for (const pack of thisModulesPacks) {
-  assert.ok(typeof pack.id === 'string' && pack.id.length);
+  for (const pack of thisModulesPacks) {
+    assert.ok(typeof pack.id === 'string' && pack.id.length);
 
-  // LH needs the pack id to match the path for createMessageInstanceIdFn
-  assert.equal(pack.id, require(`../packs/${pack.id}`).id);
+    // LH needs the pack id to match the path for createMessageInstanceIdFn
+    assert.equal(pack.id, require(`../packs/${pack.id}`).id);
 
-  assert.ok(typeof pack.title === 'string' && pack.title.length);
-  assert.ok(typeof pack.icon === 'string' && pack.icon.length);
-  assert.ok(pack.icon.startsWith('data:image/svg+xml,'));
+    assert.ok(typeof pack.title === 'string' && pack.title.length);
+    assert.ok(typeof pack.icon === 'string' && pack.icon.length);
+    assert.ok(pack.icon.startsWith('data:image/svg+xml,'));
 
-  assert.ok(Object.entries(pack.UIStrings).length);
-}
+    assert.ok(Object.entries(pack.UIStrings).length);
+  }
+});
+
+test.run();
