700873896d7d3800148e2e65a5d9fcea8b49b011
Paul Irish <commits@paul.irish>
2020-12-08 12:54:57 -0800

    tweak

--- test/lh-compat-test.js
+++ test/lh-compat-test.js
@@ -5,10 +5,19 @@ const {stackPacksToInclude: lhStackPackRefs} = require('lighthouse/lighthouse-co
 
 const packs = require('../');
 
+// LH needs the pack id to match the filepath (for now, at least)
+// https://github.com/GoogleChrome/lighthouse/blob/2cbbd289be33a586462ed248179df7d7b692ca53/lighthouse-core/lib/stack-packs.js#L84-L86
+test('pack Ids match paths', () => {
+  for (const pack of packs) {
+    const requiredPack = require(`../packs/${pack.id}`);
+    assert.equal(pack.id, requiredPack.id);
+  }
+});
+
 test('all LH referenced packs exist here', () => {
   for (const lhStackPack of lhStackPackRefs) {
     const matchedPack = packs.find(pack => pack.id === lhStackPack.packId);
-    assert.ok(matchedPack, `pack for ${lhStackPack.packId} found here`);
+    assert.ok(matchedPack, `pack for ${lhStackPack.packId} not found here`);
     assert.equal(matchedPack.id, lhStackPack.packId);
   }
 });
--- test/module-test.js
+++ test/module-test.js
@@ -20,9 +20,6 @@ test('packs are well-structured', () => {
   for (const pack of thisModulesPacks) {
     assert.ok(typeof pack.id === 'string' && pack.id.length);
 
-    // LH needs the pack id to match the path for createMessageInstanceIdFn
-    assert.equal(pack.id, require(`../packs/${pack.id}`).id);
-
     assert.ok(typeof pack.title === 'string' && pack.title.length);
     assert.ok(typeof pack.icon === 'string' && pack.icon.length);
     assert.ok(pack.icon.startsWith('data:image/svg+xml,'));
